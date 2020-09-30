5832c77fd8f50187d1a1dc442c8a9399033895ab
parsley1400 <topthecap@gmail.com>
2020-09-30 15:08:55 +0900

    Initial Commit

---
+++ ThreeSquared/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThreeSquared
+//
+//  Created by risako takeya on 2020/09/30.
+//  Copyright © 2020 Paseri Takeya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
